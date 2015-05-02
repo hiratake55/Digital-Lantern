e6ed92c4881be1fad4c17a806f1502f7bdeb47cf
takekatsu.hiramura <takekatsu.hiramura@gmail.com>
2015-05-02 16:16:04 +0900

    added

---
+++ README.md
@@ -0,0 +1,15 @@
+Digital Lantern made with Adafruit Gemma
+==============
+
+### What is this
+A Digital Lantern program for [Adafruit Gemma](http://www.adafruit.com/product/1222) and [NeoPixel Ring](http://www.adafruit.com/products/1463).
+
+### Requirements
+ + [Adafruit Arduino 1.6.x IDE](https://learn.adafruit.com/adafruit-arduino-ide-setup/arduino-1-dot-6-x-ide)
+ + [Adafruit Windows Driver](https://learn.adafruit.com/adafruit-arduino-ide-setup/windows-setup)
+ + [Adafruit NeoPixel library](https://github.com/adafruit/Adafruit_NeoPixel)
+
+### See also
+ + Setting up with Arduino IDE https://learn.adafruit.com/introducing-trinket/setting-up-with-arduino-ide
+ + Button Cycler sample https://github.com/adafruit/Adafruit_NeoPixel/blob/master/examples/buttoncycler/buttoncycler.ino
+
---
+++ pgm.ino
@@ -0,0 +1,95 @@
+#include <Adafruit_NeoPixel.h>
+
+#define BUTTON_PIN   2    // Digital IO pin connected to the button.  This will be
+                          // driven with a pull-up resistor so the switch should
+                          // pull the pin to ground momentarily.  On a high -> low
+                          // transition the button press logic will execute.
+
+#define PIXEL_PIN    0    // Digital IO pin connected to the NeoPixels.
+
+#define PIXEL_COUNT 12
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+bool oldState = HIGH;
+int showType = 0;
+
+void setup() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  // Start
+  rainbowCycle(10);
+  rainbow(20);
+  delay(5000);
+  for(;;){
+   candle(10);
+  }
+}
+
+
+// Rainbow
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Candle
+void candle(uint8_t wait) {
+  uint16_t i;
+
+  for(i=0; i<256; i++) {
+    colorWipe(strip.Color(255, 153, 0), 0);
+    colorWipe(strip.Color(255, 136, 0), 0);
+    colorWipe(strip.Color(255, 119, 0), 0);
+    colorWipe(strip.Color(255, 102, 0), 0);
+    delay(wait);
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+}
